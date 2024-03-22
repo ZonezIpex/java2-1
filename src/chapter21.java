bbb6144c8e7665bedd4ab8ba15e81f2db5d71581
ZonezIpex <aksvmfkd3@naver.com>
2024-03-22 12:48:44 +0900

    3월 22일 최종 정리본

---
+++ src/chapter21.java
@@ -0,0 +1,8 @@
+public class chapter21 {
+    // 예제2-1
+    public static void main(String[] args) {
+        public static  int sum(int n, int m) {
+            s = sum(1, 10);
+        }
+    }
+}
